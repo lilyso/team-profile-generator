7c29bdc4824403d576d40ec2eb155b0817d21c9e
lilys0 <lsouryaphanh@gmail.com>
2021-10-29 19:19:39 +1100

    questions created

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
-// const Employee = require("../lib/Employee.js");
-// const Engineer = require("../lib/Engineer.js");
-
-// // Packages needed for this application
-// const inquirer = require("inquirer");
-
-// const fs = require("fs");
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 // const content = require("./src/generate-html.js");
-// // Array of questions for user input
-// const questions = [
-//   {
-//     name: "title",
-//     message: "What is the title of your project?",
-//     type: "input",
-//   },
-//   {
-//     name: "summary",
-//     message: "Enter a short summary of your project...",
-//     type: "input",
-//   },
-//   {
-//     name: "license",
-//     message: "Choose a license:",
-//     type: "list",
-//     choices: ["MIT", "APACHE", "GPL", "LGPL", "MOZILLA"],
-//   },
-//   {
-//     name: "description",
-//     message: "Enter a description for your project...",
-//     type: "input",
-//   },
-//   {
-//     name: "installation",
-//     message: "Enter installation instructions...",
-//     type: "input",
-//   },
-//   { name: "usage", message: "Enter usage information...", type: "input" },
-//   {
-//     name: "contribute",
-//     message: "Enter contribution guidelines...",
-//     type: "input",
-//   },
-//   { name: "test", message: "Enter test instructions...", type: "input" },
-//   { name: "github", message: "What is your GitHub username?", type: "input" },
-//   { name: "githubUrl", message: "Enter your GitHub URL...", type: "input" },
-//   { name: "email", message: "What is your email?", type: "input" },
-// ];
-
-// // Write README file
+// Array of questions for user input
+const questions = {
+  type: "loop",
+  name: "employee",
+  message: "Add an employee?",
+  questions: [
+    {
+      name: "role",
+      message: "What type of employee would you like to add?",
+      type: "list",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
+      name: "name",
+      message: "Enter their full name:",
+      type: "input",
+    },
+    {
+      name: "id",
+      message: "Enter their employee id:",
+      type: "input",
+    },
+    {
+      name: "email",
+      message: "Enter their email:",
+      type: "input",
+    },
+    {
+      name: "github",
+      when: (input) => input.role === "Engineer",
+      message: "Enter their github user name",
+      type: "input",
+    },
+    {
+      name: "phone",
+      when: (input) => input.role === "Manager",
+      message: "Enter their office phone number:",
+      type: "input",
+    },
+    {
+      name: "school",
+      when: (input) => input.role === "Intern",
+      message: "Enter the school they graduated from:",
+      type: "input",
+    },
+  ],
+};
+inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
+inquirer.prompt(questions).then((data) => {
+  console.log(data);
+});
+// Write README file
 // function writeToFile(filename, data) {
 //   fs.writeFile(`${filename}.md`, data, (err) => {
 //     if (err) {
@@ -57,13 +64,14 @@
 //   });
 // }
 
-// // Initialize app - run inquirer
+// Initialize app - run inquirer
 // function init() {
 //   var prompt = inquirer.createPromptModule();
 
 //   prompt(questions).then((data) => {
-//     var readme = content(data);
-//     writeToFile("README", readme);
+//     console.log(data);
+//     // var readme = content(data);
+//     // writeToFile("index", readme);
 //   });
 // }
 
